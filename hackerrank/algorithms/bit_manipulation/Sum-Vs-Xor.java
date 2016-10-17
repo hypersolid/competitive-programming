f9b30c1693737ebd1ce99bb211510f0fcd7babe9
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-10-17 08:50:27 +0300

    hackerrank Sum vs Xor

---
+++ hackerrank/algorithms/bit_manipulation/Sum-Vs-Xor.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    long n = in.nextLong();
+    int zeroBits = 0;
+    boolean start = false;
+
+    for (int i = 63; i >= 0; i--) {
+      if ((1L << i & n) == 0) {
+        if (start) zeroBits++;
+      } else {
+        start = true;
+      }
+    }
+
+    System.out.println(1L << (zeroBits - 1) + 1);
+  }
+}
