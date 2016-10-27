7428a83b5efc545b0b75102f6fa3a4e7d5bcb9a7
Alexander Krasnoschekov <akrasnoschekov@gmail.com>
2016-10-27 18:24:50 +0300

    cf

--- codeforces/1/A.java
+++ codeforces/1/A.java
@@ -2,9 +2,9 @@ class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     int N = scanner.nextInt();
-    int M  = scanner.nextInt();
-    int A  = scanner.nextInt();
+    int M = scanner.nextInt();
+    int A = scanner.nextInt();
 
-    System.out.println(double(N * M) / A * A);
+    System.out.println(((double) N * M) / ((double) A * A));
   }
 }
