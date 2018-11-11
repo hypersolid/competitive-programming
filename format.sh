e6df06034aac5b48cf16f96137425e6789b51a84
Sol <akrasnoschekov@gmail.com>
2018-11-11 23:52:02 +0300

    adds formatter

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
-find . -name '*.go' | xargs gofmt -w
-#find . -name '*.java' | xargs java -jar ./google-java-format.jar -i
+#find . -name '*.go' | xargs gofmt -w
+find . -name '*.java' | xargs java -jar ./google-java-format.jar -i
 #find . -name '*.py' | xargs autopep8 --in-place
 #find . -name '.DS_Store' | xargs rm
