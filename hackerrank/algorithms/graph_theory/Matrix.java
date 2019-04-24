7e9e97e2816b0fc90e7425f99a2df951a052f3b6
Alexander Krasnoschekov / sol / 搜龙 <akrasnoschekov@gmail.com>
2019-04-24 09:01:06 +0300

    [master] Adds formating

--- hackerrank/algorithms/graph_theory/Matrix.java
+++ hackerrank/algorithms/graph_theory/Matrix.java
@@ -7,97 +7,101 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-    static int[] parents;
-    static boolean[] machines;
+  static int[] parents;
+  static boolean[] machines;
 
-    static int find(int source) {
-      if (source == parents[source]) { return source; }
-      int root = find(parents[source]);
-      parents[source] = root;
-      machines[source] = machines[root];
-      return root;
+  static int find(int source) {
+    if (source == parents[source]) {
+      return source;
+    }
+    int root = find(parents[source]);
+    parents[source] = root;
+    machines[source] = machines[root];
+    return root;
+  }
+
+  static void union(int source, int target) {
+    boolean infected = machines[find(source)] || machines[find(target)];
+    parents[find(source)] = find(target);
+    machines[find(source)] = infected;
+  }
+
+  static boolean joinUnlessHasTwoMachines(int source, int target) {
+    if (machines[find(source)] && machines[find(target)]) {
+      return false;
     }
 
-    static void union(int source, int target) {
-      boolean infected = machines[find(source)] || machines[find(target)];
-      parents[find(source)] = find(target);
-      machines[find(source)] = infected;
+    union(source, target);
+
+    return true;
+  }
+
+  static void sortRoads(int[][] roads) {
+    Comparator<int[]> descCostComp = (int[] row1, int[] row2) -> (row2[2] - row1[2]);
+    Arrays.sort(roads, descCostComp);
+  }
+
+  // Complete the minTime function below.
+  static int minTime(int N, int[][] roads, int[] machinesArray) {
+    sortRoads(roads);
+
+    parents = new int[N];
+    machines = new boolean[N];
+    for (int i = 0; i < N; i++) {
+      parents[i] = i;
+    }
+    for (int machineCity : machinesArray) {
+      machines[machineCity] = true;
     }
 
-    static boolean joinUnlessHasTwoMachines(int source, int target) {
-      if (machines[find(source)] && machines[find(target)]) {
-        return false;
+    int result = 0;
+    for (int[] edge : roads) {
+      if (!joinUnlessHasTwoMachines(edge[0], edge[1])) {
+        result += edge[2];
       }
-      
-      union(source, target);
-      
-      return true;
     }
+    return result;
+  }
 
-    static void sortRoads(int[][] roads) {
-      Comparator<int[]> descCostComp = (int[] row1, int[] row2) -> (row2[2] - row1[2]);
-      Arrays.sort(roads, descCostComp);
-    }
+  private static final Scanner scanner = new Scanner(System.in);
 
-    // Complete the minTime function below.
-    static int minTime(int N, int[][] roads, int[] machinesArray) {
-      sortRoads(roads);
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bufferedWriter =
+        new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-      parents = new int[N];
-      machines = new boolean[N];
-      for (int i = 0; i < N; i++) { parents[i] = i; }
-      for (int machineCity: machinesArray) { 
-        machines[machineCity] = true;
+    String[] nk = scanner.nextLine().split(" ");
+
+    int n = Integer.parseInt(nk[0]);
+
+    int k = Integer.parseInt(nk[1]);
+
+    int[][] roads = new int[n - 1][3];
+
+    for (int i = 0; i < n - 1; i++) {
+      String[] roadsRowItems = scanner.nextLine().split(" ");
+      scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+      for (int j = 0; j < 3; j++) {
+        int roadsItem = Integer.parseInt(roadsRowItems[j]);
+        roads[i][j] = roadsItem;
       }
-
-      int result = 0;
-      for (int[] edge: roads) {
-        if (!joinUnlessHasTwoMachines(edge[0], edge[1])) {
-          result += edge[2];
-        }
-      }
-      return result;
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
+    int[] machines = new int[k];
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        String[] nk = scanner.nextLine().split(" ");
-
-        int n = Integer.parseInt(nk[0]);
-
-        int k = Integer.parseInt(nk[1]);
-
-        int[][] roads = new int[n - 1][3];
-
-        for (int i = 0; i < n - 1; i++) {
-            String[] roadsRowItems = scanner.nextLine().split(" ");
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-            for (int j = 0; j < 3; j++) {
-                int roadsItem = Integer.parseInt(roadsRowItems[j]);
-                roads[i][j] = roadsItem;
-            }
-        }
-
-        int[] machines = new int[k];
-
-        for (int i = 0; i < k; i++) {
-            int machinesItem = scanner.nextInt();
-            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-            machines[i] = machinesItem;
-        }
-
-        int result = minTime(n, roads, machines);
-
-        bufferedWriter.write(String.valueOf(result));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+    for (int i = 0; i < k; i++) {
+      int machinesItem = scanner.nextInt();
+      scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+      machines[i] = machinesItem;
     }
+
+    int result = minTime(n, roads, machines);
+
+    bufferedWriter.write(String.valueOf(result));
+    bufferedWriter.newLine();
+
+    bufferedWriter.close();
+
+    scanner.close();
+  }
 }
-
--- hackerrank/algorithms/graph_theory/Roads-And-Libraries.java
+++ hackerrank/algorithms/graph_theory/Roads-And-Libraries.java
@@ -7,89 +7,91 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-    static void union(int[] forest, int node1, int node2) {
-        // if (node1 > node2) {
-        //     int tmp = node1;
-        //     node1 = node2;
-        //     node2 = tmp;
-        // }
-        forest[find(forest, node2)] = find(forest, node1);
+  static void union(int[] forest, int node1, int node2) {
+    // if (node1 > node2) {
+    //     int tmp = node1;
+    //     node1 = node2;
+    //     node2 = tmp;
+    // }
+    forest[find(forest, node2)] = find(forest, node1);
+  }
+
+  static int find(int[] forest, int node) {
+    if (node != forest[node]) {
+      forest[node] = find(forest, forest[node]);
+    }
+    return forest[node];
+  }
+
+  // Complete the roadsAndLibraries function below.
+  static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] citiesMatrix) {
+    if (c_lib <= c_road) {
+      // it's cheaper to build libraries in each city
+      return (long) c_lib * (long) n;
     }
 
-    static int find(int[] forest, int node) {
-        if (node != forest[node]) {
-            forest[node] = find(forest, forest[node]);
-        }
-        return forest[node];
+    // find trees
+    int[] forest = new int[n];
+    for (int i = 0; i < n; i++) {
+      forest[i] = i;
     }
 
-    // Complete the roadsAndLibraries function below.
-    static long roadsAndLibraries(int n, int c_lib, int c_road, int[][] citiesMatrix) {
-        if (c_lib  <= c_road) {
-            // it's cheaper to build libraries in each city
-            return (long) c_lib * (long) n;
-        }
-
-        // find trees
-        int[] forest = new int[n];
-        for (int i = 0; i < n; i++) { forest[i] = i; }
-
-        for (int[] pair: citiesMatrix) {
-            union(forest, pair[0] - 1, pair[1] - 1);
-        }
-
-        // count the costs
-        long cost = 0;
-        for (int i = 0; i < n; i++) {
-            if (find(forest, i) == i) {
-                cost += (long) c_lib;
-            } else {
-                cost += (long) c_road;
-            }
-        }
-        return cost;
+    for (int[] pair : citiesMatrix) {
+      union(forest, pair[0] - 1, pair[1] - 1);
     }
 
-    private static final Scanner scanner = new Scanner(System.in);
+    // count the costs
+    long cost = 0;
+    for (int i = 0; i < n; i++) {
+      if (find(forest, i) == i) {
+        cost += (long) c_lib;
+      } else {
+        cost += (long) c_road;
+      }
+    }
+    return cost;
+  }
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+  private static final Scanner scanner = new Scanner(System.in);
 
-        int q = scanner.nextInt();
+  public static void main(String[] args) throws IOException {
+    BufferedWriter bufferedWriter =
+        new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+    int q = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+    for (int qItr = 0; qItr < q; qItr++) {
+      String[] nmC_libC_road = scanner.nextLine().split(" ");
+
+      int n = Integer.parseInt(nmC_libC_road[0]);
+
+      int m = Integer.parseInt(nmC_libC_road[1]);
+
+      int c_lib = Integer.parseInt(nmC_libC_road[2]);
+
+      int c_road = Integer.parseInt(nmC_libC_road[3]);
+
+      int[][] cities = new int[m][2];
+
+      for (int i = 0; i < m; i++) {
+        String[] citiesRowItems = scanner.nextLine().split(" ");
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        for (int qItr = 0; qItr < q; qItr++) {
-            String[] nmC_libC_road = scanner.nextLine().split(" ");
-
-            int n = Integer.parseInt(nmC_libC_road[0]);
-
-            int m = Integer.parseInt(nmC_libC_road[1]);
-
-            int c_lib = Integer.parseInt(nmC_libC_road[2]);
-
-            int c_road = Integer.parseInt(nmC_libC_road[3]);
-
-            int[][] cities = new int[m][2];
-
-            for (int i = 0; i < m; i++) {
-                String[] citiesRowItems = scanner.nextLine().split(" ");
-                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-                for (int j = 0; j < 2; j++) {
-                    int citiesItem = Integer.parseInt(citiesRowItems[j]);
-                    cities[i][j] = citiesItem;
-                }
-            }
-
-            long result = roadsAndLibraries(n, c_lib, c_road, cities);
-
-            bufferedWriter.write(String.valueOf(result));
-            bufferedWriter.newLine();
+        for (int j = 0; j < 2; j++) {
+          int citiesItem = Integer.parseInt(citiesRowItems[j]);
+          cities[i][j] = citiesItem;
         }
+      }
 
-        bufferedWriter.close();
+      long result = roadsAndLibraries(n, c_lib, c_road, cities);
 
-        scanner.close();
+      bufferedWriter.write(String.valueOf(result));
+      bufferedWriter.newLine();
     }
-}
 
+    bufferedWriter.close();
+
+    scanner.close();
+  }
+}
